61e14d3b14614f264979eabda309941b1cb191ea
Ksrenn <karen170bayona@hotmail.com>
2024-09-07 20:08:59 -0500

    Ordenamiento (Prueba 5)

--- src/ordenamiento.py
+++ src/ordenamiento.py
@@ -1,7 +1,10 @@
 def evaluar(numero1, numero2, numero3, numero4):
     # TODO: Coloca aquí el código del ejercicio 5: Ordenamiento
-    return "";
-
+    lista_numeros = [numero1,numero2,numero3,numero4]
+    lista_numeros.sort()
+    numeros = ' '.join(map(str, lista_numeros))
+    return(numeros)
+    
 if __name__ == '__main__':
     print("Número 1:", end="")
     numero1 = int(input())
