4de9cbc9166153afd68aa1302e60a812a33128d2
Ksrenn <karen170bayona@hotmail.com>
2024-09-07 21:01:00 -0500

    Triangulo y IMC

--- src/imc.py
+++ src/imc.py
@@ -1,6 +1,18 @@
 def evaluar(peso, estatura, edad):
     # TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-    return "";
+    imc=peso/(estatura**2)
+    if (imc<22.0):
+        if(edad<45):
+            return "bajo"
+    if (imc>=22.0):
+        if(edad<45):
+            return "medio"
+    if (imc<22.0):
+        if(edad>=45):
+            return "medio"
+    if (imc>=22.0):
+        if(edad>=45):
+            return "alto"
 
 if __name__ == '__main__':
     print("Peso:", end="")
--- src/triangulo.py
+++ src/triangulo.py
@@ -1,6 +1,26 @@
 def evaluar(a, b, c):
     # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    
+    sum1=a+b
+    sum2=b+c
+    sum3=c+a
+
+    #Invalido
+    if (sum1<c) or (sum2<a) or (sum3<b):   
+        return "No es un triángulo válido"
+    
+    #Equilatero
+    if (a==b==c):
+        return "El triángulo es equilátero"
+    
+    #Isósceles
+    if (a==b or a==c or b==c):
+        return "El triángulo es isósceles"
+    
+    #Escaleno
+    return "El triángulo es escaleno"
+
+    
 
 if __name__ == '__main__':
     print("a:", end="")
