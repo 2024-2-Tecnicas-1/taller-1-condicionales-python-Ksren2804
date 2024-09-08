3c2587ac25c0ac08e0c00e4a5ec2cfaab079e1eb
Ksrenn <karen170bayona@hotmail.com>
2024-09-07 20:28:49 -0500

    Edad (Prueba 6)

--- src/edad.py
+++ src/edad.py
@@ -1,6 +1,18 @@
 def evaluar(dia, mes, anno):
     # TODO: Coloca aquí el código del ejercicio 6: Edad
-    return "";
+    from time import localtime
+    t= localtime()
+    estedia = t.tm_mday
+    estemes = t.tm_mon
+    esteanno = t.tm_year
+
+    #calcular edad
+    edadanno= esteanno-anno
+
+    #Saber si este año ya cumplio, primero los meses luego el dia
+    if(estemes,estedia)<(mes,dia):
+        edadanno -=1
+    return f"Usted tiene {edadanno} años";
 
 if __name__ == '__main__':
     print("Ingrese su fecha de nacimiento")
